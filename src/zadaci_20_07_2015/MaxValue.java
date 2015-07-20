a0a65ca18b111e27bef088df7cc70d1a6954c784
Goran <Goran@Goran-PC>
2015-07-20 17:01:57 +0200

    Zaboravio drugu stvarcicu :D

--- src/zadaci_20_07_2015/MaxValue.java
+++ src/zadaci_20_07_2015/MaxValue.java
@@ -10,17 +10,22 @@ public class MaxValue {
 	public static void main(String[] args) {
 		ArrayList<Integer> list = new ArrayList<Integer>(); // pravimo listu
 															// Integera
-		list.add(5); // dodajemo brojeve u listu
-		list.add(2);
-		list.add(3);
-		list.add(8);
-		list.add(1);
+															// list.add(5); //
+															// dodajemo brojeve
+															// u listu
+		// list.add(2);
+		// list.add(3);
+		// list.add(8);
+		// list.add(1);
 		System.out.println(max(list)); // stampamo broj koji nam metod vraca
 
 	}
 
 	public static Integer max(ArrayList<Integer> list) {
-		if (list.size() == 0 || list == null) {				//ukoliko je lista prazna ili null vraca null
+		if (list == null) { // ukoliko je lista null vraca null
+			return null;
+		}
+		if (list.size() == 0) { // ukoliko je lista prazna vraca null
 			return null;
 		}
 		Collections.sort(list); // sortiramo listu po velicini
