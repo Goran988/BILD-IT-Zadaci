1fefc55aa82a0dde99bec0be41157e16a7c144c0
Goran <Goran@Goran-PC>
2015-07-28 20:51:19 +0200

    Na sugestiju kolege Lisicica izbacio "nested loop"

--- src/zadaci_28_07_2015/TwinPrimes.java
+++ src/zadaci_28_07_2015/TwinPrimes.java
@@ -6,27 +6,34 @@ public class TwinPrimes {
 	 *         10000 in pairs 10 per line.
 	 */
 	public static void main(String[] args) {
-		int count = 0;											
-		for (int i = 1; i < 9999; i++) {										//loop that will go up to 9999
-			if (isPrime(i) && isPrime(i + 2) && i != 1) {						//checking if number i and i+2 are prime numberse
-				System.out.print("(" + i + "," + (i + 2) + ") ");				//if they both are we print them as pair
+		int count = 0;
+		for (int i = 1; i < 10000; i++) { // loop that will go up to 9999
+			if (isPrime(i) && isPrime(i + 2) && i != 1) { // checking if number
+															// i and i+2 are
+															// prime numberse
+				System.out.print("(" + i + "," + (i + 2) + ") "); // if they
+																	// both are
+																	// we print
+																	// them as
+																	// pair
 				count++;
-				if(count%10==0){												//every 10 printed pairs we start printing in next line
+				if (count % 10 == 0) { // every 10 printed pairs we start
+										// printing in next line
 					System.out.println();
 				}
 			}
 		}
 	}
 
-	public static boolean isPrime(int num) {		//method checks if the number is prime	 returns true if it is and false if it's not 
+	public static boolean isPrime(int num) { // method checks if the number is
+												// prime returns true if it is
+												// and false if it's not
 		boolean isPrime = true;
-		for (int i = 1; i <= num; i++) {
+		for (int i = 2; i < num; i++) {
 			isPrime = true;
-			for (int j = 2; j < i; j++) {
-				if (i % j == 0) {
-					isPrime = false;
-					break;
-				}
+			if (num % i == 0) {
+				isPrime = false;
+				break;
 			}
 		}
 		return isPrime;
