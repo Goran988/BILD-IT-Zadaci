51bd152f9cbf9ed9edf8d063189a6d53c266685b
Goran <Goran@Goran-PC>
2015-08-02 16:06:56 +0200

    v1.01 :D in SSN were 1-9 instead of 0-9

--- src/zadaci_02_08_2015/SocialSecurityNumber.java
+++ src/zadaci_02_08_2015/SocialSecurityNumber.java
@@ -13,7 +13,7 @@ public class SocialSecurityNumber {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter social security number(DDD-DD-DDDD format,D represents digit):");
 		String num = input.next();
-		if (num.matches("[1-9]{3}-[1-9]{2}-[0-9]{4}"))		//using regex(regular expresion) we check if the number user entered matches our desired format
+		if (num.matches("[0-9]{3}-[0-9]{2}-[0-9]{4}"))		//using regex(regular expresion) we check if the number user entered matches our desired format
 			System.out.println("Your number is valid.");	//we notify the user if number is valid or not
 		else
 			System.out.println("Your number is not valid.");
