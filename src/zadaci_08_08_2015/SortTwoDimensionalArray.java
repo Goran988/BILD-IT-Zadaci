c60d8bcb2d2b65b69ada2ed6f0861c8e61aceb1e
Goran <Goran@Goran-PC>
2015-08-14 19:18:07 +0200

    secondary sorting fixed

--- src/zadaci_08_08_2015/SortTwoDimensionalArray.java
+++ src/zadaci_08_08_2015/SortTwoDimensionalArray.java
@@ -17,22 +17,32 @@ public class SortTwoDimensionalArray {
 	}
 
 	public static void sort(int m[][]) {
-		String[] nums = new String[m.length];					//creating string array
-		Arrays.fill(nums, "");									//filling array with "" to avoid it beeing null
+		String[] nums = new String[m.length]; // creating string array
+		Arrays.fill(nums, ""); // filling array with "" to avoid it beeing null
 		for (int i = 0; i < m.length; i++) {
 			for (int j = 0; j < m[i].length; j++) {
-				nums[i] += String.valueOf(m[i][j]) + " ";		//converting rows of our 2D array into string array
+				nums[i] += String.valueOf(m[i][j]) + " "; // converting rows of
+															// our 2D array into
+															// string array
 
 			}
 		}
-		Arrays.sort(nums);										//sorting the string array
-		System.out.print("{");
-		for (int i = 0; i < nums.length; i++) {					//printing out  the elements of sorted arrays and adding brackets where needed
-			System.out.print("{" + nums[i].charAt(0) + "," + nums[i].charAt(2)
-					+ "}");
-		}
-		System.out.print("}");
+		Arrays.sort(nums); // sorting the string array
+		String[][] secondary = new String[m.length][m[0].length];
+		// converting back to 2D String array so we and performing secondary
+		// sorting(sorting of the rows)
+		for (int i = 0; i < secondary.length; i++) {
+			secondary[i] = nums[i].split(" ");
+			Arrays.sort(secondary[i]);
 
+		}
+		//printing out the new sorted 2D array
+		for (String[] e : secondary) {
+			for (String i : e) {
+				System.out.print(i + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
